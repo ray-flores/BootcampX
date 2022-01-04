e581993f2efe00416e89f5e5e11f896f597c5afe
Ray Flores <ray.m.flores@gmail.com>
2022-01-04 03:09:55 +0000

    Add Group-By and Having queries

---
+++ 3_queries_group-by/1_sums_number_of_assignments_per_day_of_bootcamp.sql
@@ -0,0 +1,5 @@
+
+SELECT day, COUNT(assignments.id) AS total_assignments
+FROM assignments
+GROUP BY day
+ORDER BY day;
---
+++ 6_queries_group-by_having/1_sums_total_assignment_submissions_per_cohort.sql
@@ -0,0 +1,7 @@
+
+SELECT cohorts.name AS cohort, COUNT(assignment_submissions.*) AS total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = assignment_submissions.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY COUNT(assignment_submissions.*) DESC; 
